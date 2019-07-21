d8c4367f806cc4be6ae13210be6b1f938ae95ae1
shruti45 <shruti45.rugi@gmail.com>
2019-07-21 14:27:24 +0530

    Initial set up

---
+++ react-native.config.js
@@ -0,0 +1,23 @@
+// react-native.config.js
+module.exports = {
+    dependencies: {
+        'react-native-gesture-handler': {
+            platforms: {
+              android: null,
+              ios: null
+            }
+          },
+    //   'react-native-google-signin': {
+    //     platforms: {
+    //       android: null, // disable Android platform, other platforms will still autolink
+    //     },
+    //   },
+    //   '@react-native-community/geolocation': {
+    //     platforms: {
+    //       android: null, // disable Android platform, other platforms will still autolink
+    //     },
+    //   },
+    },
+  };
+
+ 
